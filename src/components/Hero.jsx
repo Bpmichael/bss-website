0ccdde0c239e295d47e5c8e88d3159f6234da976
Peter Bulama <bulamagk@gmail.com>
2026-08-30 11:49:10 +0100

    Completed Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { useI18n } from "../lib/i18n.jsx";
 
 export default function Hero() {
+  const { t, lang, setLanguage } = useI18n();
+
   return (
-    <section id="Home" className="section">
-      Hero
+    <section id="Home" className="section hero">
+      <section className="container">
+        <section className="flex-wrap">
+          <section className="hero-text">
+            <h1 className="brand-name">
+              B <span>S</span> S
+            </h1>
+            <p className="hero-title">{t("hero.title")}</p>
+            <p className="hero-subtitle">{t("hero.subtitle")}</p>
+
+            <div className="hero-icons">
+              <a href="https://wa.me/+447350160962">
+                <img src="/images/whatsapp.svg" width={26} height={26} alt="" />
+              </a>
+              <a href="#">
+                <img src="/images/gmail.svg" width={26} height={26} alt="" />
+              </a>
+              <a href="#">
+                <img src="/images/facebook.svg" width={26} height={26} alt="" />
+              </a>
+            </div>
+          </section>
+          <section className="hero-img">
+            <img className="img-card" src="images/hero_img.webp" alt="" />
+          </section>
+        </section>
+      </section>
     </section>
   );
 }
